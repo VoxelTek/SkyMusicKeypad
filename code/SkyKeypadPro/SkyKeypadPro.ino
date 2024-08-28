4b9bfb61f184d1f8e69210f6f4ba54e68f665234
VoxelTek <53562267+VoxelTek@users.noreply.github.com>
2024-08-28 11:15:19 +1000

    Fix code to match relocated submodule

--- code/SkyKeypadPro/SkyKeypadPro.ino
+++ code/SkyKeypadPro/SkyKeypadPro.ino
@@ -1,7 +1,7 @@
 /**
  * This example turns the ESP32 into a Bluetooth LE keyboard that writes the words, presses Enter, presses a media key and then Ctrl+Alt+Delete
  */
-#include "src/BleKeyboard/BleKeyboard.h"
+#include "src/ESP32-BLE-Keyboard/BleKeyboard.h"
 
 BleKeyboard bleKeyboard;
 
