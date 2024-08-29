617ba5ec240bc4e8c84d8731f557f0488f19befe
VoxelTek <53562267+VoxelTek@users.noreply.github.com>
2024-08-29 14:49:54 +1000

    Code and PCB updates

--- code/SkyKeypadPro/SkyKeypadPro.ino
+++ code/SkyKeypadPro/SkyKeypadPro.ino
@@ -1,70 +1,141 @@
-/**
- * This example turns the ESP32 into a Bluetooth LE keyboard that writes the words, presses Enter, presses a media key and then Ctrl+Alt+Delete
- */
+#include <Adafruit_Keypad.h>
+#include <Adafruit_Keypad_Ringbuffer.h>
+
 #include "src/ESP32-BLE-Keyboard/BleKeyboard.h"
 
+#include "USBVendor.h"
 #include "USB.h"
 #include "USBHIDKeyboard.h"
+
+#define rotA_pin 11
+#define rotB_pin 10
+#define rotSW_pin 44
+
+USBVendor usbVendor;
+
 USBHIDKeyboard usbKeyboard;
 BleKeyboard bleKeyboard;
 
+const byte ROWS = 3;
+const byte COLS = 5;
+
+const byte rowPins[ROWS] = {8, 7, 6};
+const byte colPins[COLS] = {1, 2, 3, 4, 5};
+
+
+/*
+Mode 0 = Keyboard, BT or USB;
+Mode 1 = MIDI, BT or USB;
+*/
 int mode = 0;
-/*
-Mode 0 = Keyboard, BT or USB
-Mode 1 = MIDI, BT or USB
-*/
 
+
+/*
+Keymap 0 = Sky Music Nightly (web);
+Keymap 1 = In-game playback;
+*/
 int keymap = 0;
-/*
-Keymap 0 = Sky Music (web)
-Keymap 1 = In-game playback
-*/
 
-bool forceUSB = true;
+
+/*
+Interface Mode 0: None;
+Interface Mode 1: USB;
+Interface Mode 2: Bluetooth;
+*/
+int interface = 0;
+
+
+bool forceUSB = false;
 bool forceBT = false;
 
-bool btConnected = true;
-const int btTimeoutLength = 5;
-int btTimeout = 5;
+bool btEnabled = false;
+bool btConnected = false;
+
+bool usbStatus = false;
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting...");
 
-  if (!forceBT) {
+  pinMode(rotA_pin, INPUT_PULLUP);
+  pinMode(rotB_pin, INPUT_PULLUP);
+
+  pinMode(rotSW_pin, INPUT_PULLUP);
+
+
+  if (forceUSB) {         // Communicate over USB, no matter what
     usbKeyboard.begin();
     USB.begin();
+    interface = 1;
   }
-  if (!forceUSB) {
+  else if (forceBT) {     // Communicate over BT, no matter what
     bleKeyboard.begin();
+    btEnabled = true;
+    interface = 2;
   }
 }
 
 void loop() {
-  if (forceUSB) {
-    usbMode();
-  }
-  else if (forceBT) {
-    btMode();
-  }
-  /*
-  else if (btConnected) {
-    if (btTimeout > 0) {
-      btTimeout -= 1;
+  setMode();
+}
+
+
+
+void setMode() {
+  if (!(forceUSB || forceBT)) {
+    usbStatus = usbVendor.mounted();
+    if (usbStatus) {            // Is USB connected?
+      if (interface != 1) {     // Have we already recorded that we're in USB mode?
+        interface = 1;          // Set mode to USB
+        bleKeyboard.end();      // Disable Bluetooth while connected to USB
+        btEnabled = false;
+      }
       return;
     }
+    else {                      // Not connected to USB
+      if (!btEnabled){          // If Bluetooth is disabled, re-enable it
+        bleKeyboard.begin();
+        btEnabled = true;
+      }
+      if (bleKeyboard.isConnected()) {  // Bluetooth device connected!
+        interface = 2;
+      }
+      else {                    // No BT or USB connection
+        interface = 0;
+      }
+    }
   }
+}
 
-  Serial.println("Waiting 5 seconds...");
-  delay(5000);
-  */
+void sendKey(uint8_t key, bool press = true) {
+  if (interface == 0) {
+    return;
+  }
+  else if (interface == 1) {
+    if (press) {
+      usbKeyboard.press(key);
+    }
+    else {
+      usbKeyboard.release(key);
+    }
+  }
+  else if (interface == 2) {
+    if (press) {
+      bleKeyboard.press(key);
+    }
+    else {
+      bleKeyboard.release(key);
+    }
+  }
 }
 
 
-void usbMode() {
-  
-}
 
-void btMode() {
-  
-}
+
+
+
+/*
+TODO: mostly everything
+- add battery level (line 101 in BleKeyboard.cpp)
+- add matrix (use hex for keys?)
+*/
